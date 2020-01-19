974c7f2565d126b16e01812ef511fecc76ec0b74
adam yusuf <adamyusuf3494@gmail.com>
2020-01-19 17:08:37 -0500

    Adam Yusuf

--- src/main.js
+++ src/main.js
@@ -11,7 +11,16 @@ Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 //Firebase web app config
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAPr2_eWnMbo_uydh7Pa3m3rXE5S4-MJcg",
+  authDomain: "officialsnkaegame.firebaseapp.com",
+  databaseURL: "https://officialsnkaegame.firebaseio.com",
+  projectId: "officialsnkaegame",
+  storageBucket: "officialsnkaegame.appspot.com",
+  messagingSenderId: "150123891628",
+  appId: "1:150123891628:web:eb56914dbbbc337a820809",
+  measurementId: "G-PE2N0XC843"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -24,7 +33,7 @@ Vue.config.productionTip = false
 let app;
 
 firebase.auth().onAuthStateChanged(user=>{
-  console.log(user);
+  if(user){
   if(!app){
     app = new Vue({
       router,
@@ -32,4 +41,5 @@ firebase.auth().onAuthStateChanged(user=>{
       render: h => h(App)
     }).$mount('#app')
     }
+  }
 });
