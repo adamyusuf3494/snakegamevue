c8cf5ffbc78bce547a42c265cbd243082a359c89
adam yusuf <adamyusuf3494@gmail.com>
2020-01-19 00:22:28 -0500

    Adam Yusuf restructure folders and remove fibase web config info

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# snake-game
+# snake-game-official
 
-The snake game create using javascript, vue framwork and html5.
-This game is still in development
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -4,29 +4,27 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import firebase from 'firebase/app'
+import 'firebase/firestore'
+require('firebase/auth')
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 //Firebase web app config
-const firebaseConfig = {
-  apiKey: "AIzaSyC3h3gqRIvVSSlOQdQbatQfXdL0o_O6hwE",
-  authDomain: "snake-game-vue.firebaseapp.com",
-  databaseURL: "https://snake-game-vue.firebaseio.com",
-  projectId: "snake-game-vue",
-  storageBucket: "snake-game-vue.appspot.com",
-  messagingSenderId: "919450021262",
-  appId: "1:919450021262:web:881201449a1f901feb08df",
-  measurementId: "G-8XZ6RMYWBY"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export default db;
+
+Vue.config.productionTip = false
 
 let app;
 
 firebase.auth().onAuthStateChanged(user=>{
-  console.log(user)
+  console.log(user);
   if(!app){
     app = new Vue({
       router,
@@ -34,5 +32,4 @@ firebase.auth().onAuthStateChanged(user=>{
       render: h => h(App)
     }).$mount('#app')
     }
-})
-
+});
